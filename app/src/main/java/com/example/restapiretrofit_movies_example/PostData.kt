592f1f48abe5a885447c113388db3d1390f238ff
SapnaGulati <sapnagulati9990@gmail.com>
2021-03-19 11:06:35 +0530

    review post data

--- app/src/main/java/com/example/restapiretrofit_movies_example/PostData.kt
+++ app/src/main/java/com/example/restapiretrofit_movies_example/PostData.kt
@@ -4,9 +4,11 @@ import android.os.Bundle
 import android.text.TextUtils
 import android.util.Log
 import android.view.View
+import android.view.View.VISIBLE
 import android.widget.Button
 import android.widget.EditText
 import android.widget.TextView
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.example.restapiretrofit_movies_example.data.model.Post
 import com.example.restapiretrofit_movies_example.data.remote.APIService
@@ -30,7 +32,7 @@ class PostData : AppCompatActivity() {
         val data = findViewById<EditText>(R.id.data)
         val btn = findViewById<Button>(R.id.button)
         textView = findViewById(R.id.textView)
-        apiService = ApiUtils.getAPIService()!!
+        apiService = ApiUtils.getAPIService()
 
         btn.setOnClickListener {
             val title = title.text.toString().trim()
@@ -48,22 +50,33 @@ class PostData : AppCompatActivity() {
                 if (response.isSuccessful) {
                     showResponse(response.body().toString())
                     Log.i("TAG", "Post submitted successfully" + response.body().toString())
+                    Toast.makeText(applicationContext, "Data submitted successfully", Toast.LENGTH_SHORT).show()
+                    textView.visibility = VISIBLE
+                    textView.text = response.toString()
                 }
             }
 
             override fun onFailure(call: Call<Post?>, t: Throwable) {
-                if(call.isCanceled) {
-                    Log.e("TAG", "Submitting post was aborted")
+                try {
+                    if (call.isCanceled) {
+                        Log.e("TAG", "Submitting post was aborted")
+                        Toast.makeText(applicationContext, "Submitting post was aborted", Toast.LENGTH_SHORT).show()
+                        textView.visibility = VISIBLE
+                        textView.text = "Submitting post was aborted"
+                    }
+                    else {
+                        Log.e("TAG", "Submitting post un-successful")
+                        Toast.makeText(applicationContext, "Submitting post un-successful", Toast.LENGTH_SHORT).show()
+                    }
                 }
-                else {
-                    Log.e("TAG", "Submitting post un-successful")
+                catch (e: Exception) {
+                    e.printStackTrace()
+                    Toast.makeText(applicationContext, "$e", Toast.LENGTH_SHORT).show()
+                    textView.visibility = VISIBLE
+                    textView.text = e.toString()
                 }
             }
         })
-    }
-
-    fun cancelRequest() {
-        call.cancel()
     }
 
     fun showResponse(string: String) {
